9163e2dbe943bedb81edc781931040ffc5cefeca
Fabian Ananda Merdana <yanappuowo@yandex.com>
2026-04-29 00:11:03 +0700

    pertemuan 17

--- my-app/src/pages/editor/index.tsx
+++ my-app/src/pages/editor/index.tsx
@@ -2,11 +2,8 @@ export default function HalamanEditor() {
   return (
     <div>
       <div className="admin">
-        <h1>Halaman Admin</h1>
-        <p>
-          Selamat datang di halaman Editor! Anda memiliki akses tidak penuh ke
-          semua halaman
-        </p>
+        <h1>Halaman Editor</h1>
+        <p>Selamat datang di halaman Editor!</p>
       </div>
     </div>
   );
